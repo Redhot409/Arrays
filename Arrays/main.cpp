30e395dcd6951f3800eb275cfeb264e7f8a0adc0
Alexander Melikhov <alexandersermel@gmail.com>
2024-02-12 19:03:19 +0300

    Basic array partly done

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -7,26 +7,44 @@ void main()
 	setlocale(LC_ALL, "");
 	cout << "Hello Arrays" << endl;
 
-	int i;
+	float equal;
+	float sum = 0;
 	const int SIZE = 5;
 	int arr[SIZE];
 	//cout << int() << endl;
 
-	//Ввод элементов с клавиатуры
+	//Ввод элементов массива с клавиатуры
 
-	cout<< "Ввести значения элементов массива с клавиатуры :"<<"\n";
-	for(int i = 0; i<SIZE; i++)
-	{ 
-		cin>>arr[i];
-	}
-	//вывод массива на экран
+	cout<< "Введите значения элементов массива :"<<endl;
 	for (int i = 0; i < SIZE; i++)
 	{
+		cin >> arr[i];
+	 }
 
+	//Вывод элементов массива на экран
+	for (int i = 0; i < SIZE; i++)
+	{
 		cout << arr[i] << "\t";
-
 	}
 	cout << endl;
 
+	//Вывод элементов массива на экран в обратном порядке
+	for (int i = SIZE - 1; i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//Вычисление суммы элементов массива и средне-арифметической величины элементов массива
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+		equal = sum / SIZE;
+	}
+	cout <<"Array element amount is " <<sum<< endl;
+	cout <<"Equal element value is " <<equal<< endl;
+
+	
+	
 
 }
